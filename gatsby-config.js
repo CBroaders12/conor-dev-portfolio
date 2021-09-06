ffb8a252a3baefe69ff6a0c65864dd2e5fff7208
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2021-09-06 10:07:59 -0400

    fix configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Conor Dev Portfolio",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "WvTwyrs1LRO_-gRCpjOgPsxtLcM2MmiZL8r7KAq7T5Y",
-        spaceId: "6i4nt0aquy0a",
-      },
-    },
-  ],
+	siteMetadata: {
+		siteUrl: 'https://www.yourdomain.tld',
+		title: 'Conor Dev Portfolio',
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-source-contentful',
+			options: {
+				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+				spaceId: '6i4nt0aquy0a',
+			},
+		},
+	],
 };
